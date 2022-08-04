0d137ae33a8c2c85ddfbba21e1ba6dd14345f1c6
Tony Frieiro <tonyfrieiro@gmail.com>
2022-08-03 21:04:39 -0300

    Proyecto final(1)

--- script.js
+++ script.js
@@ -7,20 +7,26 @@ let salir = 0;
 
 const array = [];
 
+function saliendo(){
+    salir = prompt("desea salir escriba 1")
+}
+
 while (salir != 1){
     let usuario = prompt ("ingrese el usuario")
     console.log(usuario)
     let clave = prompt ("ingrese la clave")
     console.log(clave)
-
+    
     const persona1 = new Persona (usuario, clave);
-
+    
     array.push(persona1)
 
+    console.log(array)
     console.log(array.toString());
     console.log(array.length);
+    console.log(Object.keys(array))
 
-    salir = prompt("desea salir escriba 1")
+    saliendo();
 }
 
 
