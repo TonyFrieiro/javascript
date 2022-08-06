d39c280327fd25213cf86b8b0aab1e31361cc539
Tony Frieiro <tonyfrieiro@gmail.com>
2022-08-05 22:41:25 -0300

    interactuar con HTML

--- script.js
+++ script.js
@@ -7,26 +7,83 @@ let salir = 0;
 
 const array = [];
 
-function saliendo(){
-    salir = prompt("desea salir escriba 1")
-}
+const boton = document.getElementById("boton")
 
-while (salir != 1){
-    let usuario = prompt ("ingrese el usuario")
-    console.log(usuario)
-    let clave = prompt ("ingrese la clave")
-    console.log(clave)
-    
+
+boton.addEventListener("click",()=>{
+    let usuario = document.getElementById("usuario").value;
+    let clave = document.getElementById("password").value;
+
     const persona1 = new Persona (usuario, clave);
-    
+
     array.push(persona1)
 
     console.log(array)
-    console.log(array.toString());
-    console.log(array.length);
-    console.log(Object.keys(array))
+    if ((usuario == "tony")&&(clave == "1234")){
+        let formulario = document.getElementById("formulario");
+    
+        formulario.remove();
+    
+        let bienvenido = document.createElement("p")
+        bienvenido.innerHTML = "<h2> Bienvenido a la pagina " + usuario + " , lograste iniciar sesion </h2>"
+        document.body.append(bienvenido);
+    }
+    
+})
 
-    saliendo();
-}
+
+
+// function saliendo(){
+//     salir = prompt("desea salir escriba 1")
+// }
+
+// while (salir != 1){
+//     //let usuario = prompt ("ingrese el usuario")
+//     //console.log(usuario)
+//     //let clave = prompt ("ingrese la clave")
+//     //console.log(clave)
+
+//     let usuario = document.getElementById("usuario");
+//     let clave = document.getElementById("password");
+
+
+    
+//     const persona1 = new Persona (usuario, clave);
+    
+//     array.push(persona1)
+    
+//     console.log(array)
+//     console.log(array.toString());
+//     console.log(array.length);
+//     console.log(Object.keys(array))
+
+//     //saliendo();
+// }
+
+
+
+// let usuario = document.getElementById("usuario");
+// let clave = document.getElementById("password");
+
+// const persona1 = new Persona (usuario, clave);
+
+// array.push(persona1)
+
+// console.log(array)
+// console.log(array.toString());
+// console.log(array.length);
+// console.log(Object.keys(array))
+
+
+// function ejecutar(Persona){
+//     let usuario = document.getElementById("usuario");
+//     let clave = document.getElementById("password");
+
+//     const persona1 = new Persona (usuario, clave);
+
+//     array.push(persona1)
+
+//     console.log(array)
+// }
 
 
